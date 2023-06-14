0a549a4938455e8d2c663abcc9c5d2e7cfaf4894
Anderson Fernandes <anderson.fndss@gmail.com>
2023-06-14 12:29:17 -0300

    last cards

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -23,4 +23,29 @@ export const dataFake = [
     "description": "Segundo Charlie Brooker, o sistema apenas criou um roteiro misturando informações das sinopses da série, sem adicionar “nenhum pensamento original”. A ferramenta, no entanto, o ajudou a perceber como poderia subverter as expectativas do que é esperado de um episódio da série. As informações são do site TechCrunch.",
     "photoCover": "https://th.bing.com/th/id/R.0ad60c0d2782f223001852af61042c2d?rik=myQqzDN%2bZOZtfA&riu=http%3a%2f%2fksassets.timeincuk.net%2fwp%2fuploads%2fsites%2f55%2f2017%2f11%2f2017_CharlieBrooker_Rex_291117.jpg&ehk=c5Ebm0eKj9hbtY4Nj5qOIqBqJodfmZ44Oykg7xkOtBE%3d&risl=&pid=ImgRaw&r=0"
   }
+  ,
+  {
+    "id": "5",
+    "title": "Audiência do Stack Overflow despenca por conta da IA.",
+    "description": "É o fim de uma era. Durante quase quinze anos, o Stack Overflow foi o principal destino dos programadores que tinham alguma dúvida na hora de desenvolver seu código. Desde janeiro desse ano, a audiência do serviço vem caindo mês a mês. Em média, é uma queda de 6% no tráfego para o site, porém março registrou uma redução de mais de 13% no volume de usuários. Além de desenvolvedores do mundo todo, empresas como Microsoft, Bloomberg, Instacart e outras são clientes do Stack Overflow. Ainda que flutuações de acesso possam ser atribuídas a múltiplos fatores, a explicação mais aceita nesse momento é a ascensão dos assistentes de código inteligentes, como o GitHub Copilot e o CodeWhisperer. ",
+    "photoCover": "https://jessehouwing.net/content/images/size/w2000/2018/07/stackoverflow-1.png"
+  },
+  {
+    "id": "6",
+    "title": "IA Generativa traz investimento para startups.",
+    "description": "O outro lado da moeda das IAs é o surgimento de novas startups. No primeiro trimestre de 2023, as startups de IA generativa arrecadaram impressionantes US$ 1,7 bilhão em investimentos. Além disso, novos aportes estão chegando no valor de mais de US$ 10 bilhões. O território ainda não foi completamente dominado: existem grandes nomes como a OpenAI e a Anthropic, mas os investidores de capital de risco estão de olho em novidades e uma delas é a startup Together. A startup desenvolve IA generativa de código aberto e anunciou uma arrecadação US$ 20 milhões em uma rodada inicial. Esse primeiro movimento foi liderado pela Lux Capital, com a participação de várias outras empresas de investimento. ",
+    "photoCover": "https://content.news.ifood.com.br/uploads/2023/01/capa_aigenerative.jpg"
+  },
+  {
+    "id": "7",
+    "title": "CEO da OpenAI depõe no Congresso americano e concorda com necessidade de regulação.",
+    "description": "Sam Altman, o CEO da OpenAI, encarou uma barra-pesada essa semana: uma sabatina no Senado dos Estados Unidos para debater a necessidade de regulação das Inteligências Artificiais. O executivo não chegou a ser metralhado de perguntas pelos senadores, como outros executivos de tecnologia no passado. Ainda assim, Altman precisou responder algumas questões espinhosas e chegou a ser instruído a não se desviar do tema. Perguntado sobre qual seria seu maior temor a respeito das IAs, Altman acabou declarando: “o pior medo é que nós - o campo, a tecnologia, a indústria - causemos danos significativos ao mundo”. A sua boa-vontade chegou a ser elogiada pelos presentes. ",
+    "photoCover": "https://maiconramos.com/wp-content/uploads/2023/03/capa-post-openai.png"
+  },
+  {
+    "id": "8",
+    "title": "O Twitter vai mesmo processar a Microsoft?",
+    "description": "Elon Musk postou na rede social que suspeitava que a Microsoft teria utilizado indevidamente dados do Twitter para alimentar seus modelos de IA. Foi um tuíte provocativo, que parecia que não daria em nada. Mas deu: Alex Spiro, advogado pessoal de Musk, enviou uma carta formal para Satya Nadella, CEO da Microsoft. Spiro acusa a outra empresa de ter violado múltiplas vezes os termos de uso da rede social, de forma constante e continuada. O advogado pede que seja realizada uma auditoria dos últimos dois anos em 8 produtos da Microsoft que teriam feito uso não-autorizado das APIs do Twitter. Por enquanto, não há uma ameaça legal direta na carta, mas o clima ficou estranho entre as duas empresas.",
+    "photoCover": "https://images.gizbot.com/fit-in/img/600x338/2023/04/elon-musk-sue-microsoft-chatgpt-1681990550.jpg"
+  }
 ]
